86e570c9ff511f3a37a503c99005cca9486f9568
Luigysenger <SENGER.LUIGY@GMAIL.COM>
2026-09-06 20:01:15 -0300

    Update effects.js

--- effects.js
+++ effects.js
@@ -1,1 +1,48 @@
+export function playSuccessSound() {
+    try {
+        const audio = new Audio("sons/acerto.mp3");
+        audio.volume = 0.7;
+        audio.play();
+    } catch (e) {}
+}
 
+export function playErrorSound() {
+    try {
+        const audio = new Audio("sons/erro.mp3");
+        audio.volume = 0.7;
+        audio.play();
+    } catch (e) {}
+}
+
+export function showConfetti() {
+    if (window.confetti) {
+        confetti({
+            particleCount: 180,
+            spread: 120,
+            origin: { y: 0.6 }
+        });
+    }
+}
+
+export function showRain() {
+    if (window.confetti) {
+        confetti({
+            particleCount: 220,
+            angle: 90,
+            spread: 45,
+            startVelocity: 70,
+            gravity: 2,
+            origin: { x: 0.5, y: 0 }
+        });
+    }
+}
+
+export function celebrateVictory() {
+    playSuccessSound();
+    showConfetti();
+}
+
+export function celebrateDefeat() {
+    playErrorSound();
+    showRain();
+}
