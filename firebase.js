36d44d4600456f6daefa4f1782f898b6408738d4
Luigysenger <SENGER.LUIGY@GMAIL.COM>
2026-09-06 20:02:19 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
+import {
+    getDatabase,
+    ref,
+    get,
+    set,
+    update,
+    push,
+    remove,
+    onValue
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_AUTH_DOMAIN",
+    databaseURL: "SEU_DATABASE_URL",
+    projectId: "SEU_PROJECT_ID",
+    storageBucket: "SEU_STORAGE_BUCKET",
+    messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+    appId: "SEU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+
+export {
+    ref,
+    get,
+    set,
+    update,
+    push,
+    remove,
+    onValue
+};
