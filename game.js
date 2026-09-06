1f99354de4f014d7c0480d4afc35adeebc675b74
Luigysenger <SENGER.LUIGY@GMAIL.COM>
2026-09-06 20:15:46 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,29 @@
+import { QUESTIONS } from "./questions.js";
+import { db } from "./firebase.js";
+import {
+    shuffle,
+    randomCode,
+    uid,
+    sleep,
+    copy,
+    capitalize
+} from "./utils.js";
 
+import {
+    playCorrect,
+    playWrong,
+    confettiExplosion,
+    rainExplosion
+} from "./effects.js";
+
+// TODO:
+// Toda a lógica do jogo ficará aqui.
+// Salas
+// Técnicos
+// Jogadores
+// Perguntas
+// Cronômetro
+// Melhor de 3
+// Pontuação
+// Firebase
+// etc.
