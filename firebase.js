c884f80056f488888634152c33c513b7c83fca9c
Luigysenger <SENGER.LUIGY@GMAIL.COM>
2026-09-06 21:28:43 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 
 import {
     getDatabase,
@@ -6,31 +7,41 @@ import {
     get,
     set,
     update,
-    push,
     remove,
-    onValue
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
+    onValue,
+    runTransaction,
+    onDisconnect,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_AUTH_DOMAIN",
-    databaseURL: "SEU_DATABASE_URL",
-    projectId: "SEU_PROJECT_ID",
-    storageBucket: "SEU_STORAGE_BUCKET",
-    messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-    appId: "SEU_APP_ID"
+    apiKey: "AIzaSyD_-S0j1L-YVF6ynKiF8NcTU_4_pCn-S3Q",
+    authDomain: "jogo-dos-amigos-ca3f5.firebaseapp.com",
+    databaseURL: "https://jogo-dos-amigos-ca3f5-default-rtdb.firebaseio.com",
+    projectId: "jogo-dos-amigos-ca3f5",
+    storageBucket: "jogo-dos-amigos-ca3f5.firebasestorage.app",
+    messagingSenderId: "817678231030",
+    appId: "1:817678231030:web:1e406c32d15e0ec7a953d7"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app);
+const auth = getAuth(app);
+
+const db = getDatabase(app);
+
+export { app, auth, db };
 
 export {
     ref,
     get,
     set,
     update,
-    push,
     remove,
-    onValue
+    onValue,
+    runTransaction,
+    onDisconnect,
+    serverTimestamp,
+    signInAnonymously,
+    onAuthStateChanged
 };
